aeadf4c6f8cdc114506ecc4f2f730272b31b66bc
cyee0011 <cyee0011@student.monash.edu>
2020-10-19 12:21:29 +0800

    update data in readme rmd file

--- R/coronavirus.R
+++ R/coronavirus.R
@@ -1,9 +1,22 @@
-#' General Situation of COVID-19 in the world
+#' General Situation of corona virus in the World
 #'
-#' This dataset contains daily information of COVID-19 about 188 countries.
-#'
-#' @source 
-#' coronavirus
+#' A dataset containing general information of corona virus about 188 countries. The 
+#' information includes daily confirmed cases, death and recovered cases.
+#' 
+#' @format a tibble with 181335 observations and 7 variables:
+#' 
+#' - **date**: The date of the summary
+#' - **province**: The province or state, when applicable
+#' - **country**: The country or region name
+#' - **lat**: Latitude point
+#' - **long**: Longitude point
+#' - **type**: the type of case (i.e., confirmed, death)
+#' - **cases**: the number of daily cases (corresponding to the case type)
+#' 
+#' 
+#' @import tibble
+#' 
+#' @source [WHO](https://www.who.int/) and [coronavirus](https://github.com/RamiKrispin/coronavirus)
 "coronavirus"
 
 
