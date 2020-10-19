5c7db863210a4c0762490eb4359b533cadd99ce5
cyee0011 <cyee0011@student.monash.edu>
2020-10-19 11:09:08 +0800

    update the man file and document

--- R/coronavirus.R
+++ R/coronavirus.R
@@ -1,12 +1,9 @@
 #' General Situation of COVID-19 in the world
 #'
+#' This dataset contains daily information of COVID-19 about 188 countries.
 #'
-#'
-#'
-#'
-#'
-#'
-#'
+#' @source 
+#' coronavirus
 "coronavirus"
 
 
