8d288636c9a217781fc45b1cfb5f3ab39cfd5719
cyee0011 <cyee0011@student.monash.edu>
2020-10-21 14:31:56 +0800

    change tidyverse to dplyr and tidyr

--- R/Easykable.R
+++ R/Easykable.R
@@ -8,7 +8,6 @@
 #'
 #' @importFrom magrittr %>% 
 #'
-#' @importFrom utils head
 #'
 #' @export
 
@@ -19,7 +18,7 @@ Easykable <- function(n_country)
    dplyr::summarise(total_cases = sum(cases)) %>%
    tidyr::pivot_wider(names_from = type, values_from = total_cases) %>%
    dplyr::arrange(-confirmed) %>%
-   head(n_country) %>% 
+   utils::head(n_country) %>% 
    kableExtra::kable(align = 'lccr', 
                      format.args = list(big.mark = ",")) %>% 
    kableExtra::kable_styling(
--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 
 <!-- badges: start -->
 
+[![R build
+status](https://github.com/etc5523-2020/r-package-assessment-Year-97/workflows/R-CMD-check/badge.svg)](https://github.com/etc5523-2020/r-package-assessment-Year-97/actions)
 <!-- badges: end -->
 
 The goal of CovidYe is to provide some basic information about COVID-19
